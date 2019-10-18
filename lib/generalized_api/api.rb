ea1df858a84f3d52b81471300e2f2c1faf302fcd
Gregory Havenga <gregory.havenga@gmail.com>
2019-10-18 10:23:02 +0200

    Experiement with callback architecture in module implementation

--- lib/generalized_api/api.rb
+++ lib/generalized_api/api.rb
@@ -4,79 +4,238 @@ require "will_paginate"
 
 module GeneralizedApi
   module Api
+    extend ActiveSupport::Concern
     @@callbacks = {}
 
+    # def index
+    #   query = resource.where(permitted_params).order(order_param).paginate(pagination_params)
+
+    #   query = yield query if block_given? 
+
+    #   body = {resource_key.pluralize => query}
+
+    #   render_processed_entity(body)
+    # end
+
+    # def count
+    #   query = resource.where(permitted_params).count
+  
+    #   query = yield query if block_given? 
+
+    #   body = {resource_key.pluralize + '_count' => query}
+
+    #   render_processed_entity(body)
+    # end
+
+    # def show
+    #   operate_on_valid_object do |object|
+    #     object = yield object if block_given? 
+    #     render_processed_entity(resource_key => object)
+    #   end
+    # end
+
+    # def create
+    #   object = resource.new(permitted_params)
+    #   object = yield object if block_given? 
+    #   render_processed_entity(resource_key => object) && return if object.save
+    #   render_unprocessable_entity(messages: object.errors.full_messages.uniq)
+    # end
+
+    # def destroy
+    #   operate_on_valid_object do |object|
+    #     if object.destroy
+    #       object = yield object if block_given? 
+    #       render_processed_entity(message: "#{resource_key} With ID #{params[:id]} Succesfully Deleted")
+    #       return
+    #     else
+    #       render_unprocessable_entity(messages: object.errors.full_messages.uniq)
+    #     end
+    #   end
+    # end
+
+    # def update
+    #   operate_on_valid_object do |object|
+    #     object.update(permitted_params)
+    #     if object.save
+    #       object = yield object if block_given? 
+    #       render_processed_entity(resource_key => object)
+    #       return
+    #     else
+    #       render_unprocessable_entity(messages: object.errors.full_messages.uniq)
+    #     end
+    #   end
+    # end
+
+    # def search
+    #   if params["search_field"] && params["search_string"]
+    #     query = resource.where(permitted_params).where(fuzzy_search_field, fuzzy_search_query).order(order_param).paginate(pagination_params)
+    #     query = yield query if block_given? 
+
+    #     render_processed_entity(resource_key.pluralize => query)
+    #   else
+    #     render_unprocessable_entity(messages: "Please select a 'search_field' and a 'search_string'")
+    #   end
+    # end
+
     def index
-      query = resource.where(permitted_params).order(order_param).paginate(pagination_params)
-
-      query = yield query if block_given? 
-
-      body = {resource_key.pluralize => query}
-
-      render_processed_entity(body)
+      callback_structure do
+        query = resource.where(permitted_params).order(order_param).paginate(pagination_params)
+        query = yield query if block_given? 
+        query = filters(query)
+        body = {resource_key.pluralize => query}
+        render_processed_entity(body)
+      end
     end
 
     def count
-      query = resource.where(permitted_params).count
-  
-      query = yield query if block_given? 
-
-      body = {resource_key.pluralize + '_count' => query}
-
-      render_processed_entity(body)
+      callback_structure do
+        query = resource.where(permitted_params).count
+        query = yield query if block_given? 
+        query = filters(query)
+        body = {resource_key.pluralize + '_count' => query}
+        render_processed_entity(body)
+      end
     end
 
     def show
-      operate_on_valid_object do |object|
-        object = yield object if block_given? 
-        render_processed_entity(resource_key => object)
+      callback_structure do
+        operate_on_valid_object do |object|
+          object = yield object if block_given? 
+          object = filters(object)
+          render_processed_entity(resource_key => object)
+        end
       end
     end
 
     def create
-      object = resource.new(permitted_params)
-      object = yield object if block_given? 
-      render_processed_entity(resource_key => object) && return if object.save
-      render_unprocessable_entity(messages: object.errors.full_messages.uniq)
+      callback_structure do
+        object = resource.new(permitted_params)
+        object = yield object if block_given? 
+        object = filters(object)
+        render_processed_entity(resource_key => object) && return if object.save
+        render_unprocessable_entity(messages: object.errors.full_messages.uniq)
+      end
     end
 
     def destroy
-      operate_on_valid_object do |object|
-        if object.destroy
-          object = yield object if block_given? 
-          render_processed_entity(message: "#{resource_key} With ID #{params[:id]} Succesfully Deleted")
-          return
-        else
-          render_unprocessable_entity(messages: object.errors.full_messages.uniq)
+      callback_structure do
+        operate_on_valid_object do |object|
+          if object.destroy
+            object = yield object if block_given? 
+            object = filters(object)
+            render_processed_entity(message: "#{resource_key} With ID #{params[:id]} Succesfully Deleted")
+            return
+          else
+            render_unprocessable_entity(messages: object.errors.full_messages.uniq)
+          end
         end
       end
     end
 
     def update
-      operate_on_valid_object do |object|
-        object.update(permitted_params)
-        if object.save
-          object = yield object if block_given? 
-          render_processed_entity(resource_key => object)
-          return
-        else
-          render_unprocessable_entity(messages: object.errors.full_messages.uniq)
+      callback_structure do
+        operate_on_valid_object do |object|
+          object.update(permitted_params)
+          if object.save
+            object = yield object if block_given? 
+            object = filters(object)
+            render_processed_entity(resource_key => object)
+            return
+          else
+            render_unprocessable_entity(messages: object.errors.full_messages.uniq)
+          end
         end
       end
     end
 
     def search
-      if params["search_field"] && params["search_string"]
-        query = resource.where(permitted_params).where(fuzzy_search_field, fuzzy_search_query).order(order_param).paginate(pagination_params)
-        query = yield query if block_given? 
+      callback_structure do
+        if params["search_field"] && params["search_string"]
+          query = resource.where(permitted_params).where(fuzzy_search_field, fuzzy_search_query).order(order_param).paginate(pagination_params)
+          query = yield query if block_given? 
+          query = filters(query)
 
-        render_processed_entity(resource_key.pluralize => query)
-      else
-        render_unprocessable_entity(messages: "Please select a 'search_field' and a 'search_string'")
+          render_processed_entity(resource_key.pluralize => query)
+        else
+          render_unprocessable_entity(messages: "Please select a 'search_field' and a 'search_string'")
+        end
+      end
+    end
+
+    included do
+      def self.permit_params(param)
+        @@resource_params ||= {}
+        case param
+        when Hash
+          param.each do |key, array|
+            throw "Paramter Array expected, recieved #{array.class}" if array.class != Array
+            array.each do |value|
+              throw "Invalid parameter type #{value.class} for model #{key.to_s.titleize}" if value.class != String && value.class != Symbol
+              @@resource_params[key.to_s.singularize.to_sym] ||= []
+              @@resource_params[key.to_s.singularize.to_sym] << value.to_sym
+            end
+          end
+        when Array
+          param.each do |value|
+            @@resource_params[self.to_s.tableize.split('_controllers').first.singularize] ||= []
+            @@resource_params[self.to_s.tableize.split('_controllers').first.singularize] << value.to_sym
+          end
+        when Symbol
+          @@resource_params[self.to_s.tableize.split('_controllers').first.singularize] ||= []
+          @@resource_params[self.to_s.tableize.split('_controllers').first.singularize] << param.to_sym
+        when String
+          @@resource_params[self.to_s.tableize.split('_controllers').first.singularize] ||= []
+          @@resource_params[self.to_s.tableize.split('_controllers').first.singularize] << param.to_sym
+        else
+          throw "Unpermittable parameter class #{param.class}"
+        end
+      end
+
+      def self.before_action(method_name, options={})
+        @@callbacks[self] ||= {before: [], after: [], filter: []}
+        @@callbacks[self][:before] << method_name.to_sym
+      end
+
+      def self.apply_filter(method_name, options={})
+        @@callbacks[self] ||= {before: [], after: [], filter: []}
+        @@callbacks[self][:filter] << method_name.to_sym
+      end
+
+      def self.after_action(method_name, option={})
+        @@callbacks[self] ||= {before: [], after: [], filter: []}
+        @@callbacks[self][:after] << method_name.to_sym
       end
     end
 
     private
+
+    def callback_structure
+      before_callbacks
+      yield
+      after_callbacks
+    end
+
+    def filters(information)
+      return information unless @@callbacks[self.class]
+      @@callbacks[self.class][:filter].each do |method_name|
+        information = self.send(method_name, information)
+      end
+      information
+    end
+
+    def before_callbacks
+      return unless @@callbacks[self.class]
+      @@callbacks[self.class][:before].each do |method_name|
+        self.send(method_name)
+      end
+    end
+
+    def after_callbacks
+      return unless @@callbacks[self.class]
+      @@callbacks[self.class][:after].each do |method_name|
+        self.send(method_name)
+      end
+    end
 
     def fuzzy_search_field
       "#{params["search_field"]} #{GeneralizedApi::DATABASE_LIKE} ?"
